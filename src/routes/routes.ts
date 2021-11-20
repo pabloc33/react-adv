cc899e210b12bff49a6c4f9e0c0e89d45dbeef83
pabloc33 <pablo.contino33@gmail.com>
2021-11-19 21:58:53 -0300

    Lazyload basico listo

---
+++ src/routes/routes.ts
@@ -0,0 +1,53 @@
+import { LazyExoticComponent } from "react";
+import { lazy } from "react";
+
+type JSXComponent = () => JSX.Element;
+
+interface Route {
+  path: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent; // () => JSX.Element;
+  name: string;
+  children?: Route[];
+}
+
+const LazyPage1 = lazy(
+  () =>
+    import(
+      /*  webpackChunkName: "LazyPage1" */ "../01-lazyload/pages/LazyPage1"
+    )
+);
+const LazyPage2 = lazy(
+  () =>
+    import(
+      /*  webpackChunkName: "LazyPage2" */ "../01-lazyload/pages/LazyPage2"
+    )
+);
+const LazyPage3 = lazy(
+  () =>
+    import(
+      /*  webpackChunkName: "LazyPage3" */ "../01-lazyload/pages/LazyPage3"
+    )
+);
+
+export const routes: Route[] = [
+  {
+    path: "/lazy1",
+    Component: LazyPage1,
+    name: "LazyPage-1",
+  },
+  {
+    path: "/lazy2",
+    Component: LazyPage2,
+    name: "LazyPage-2",
+  },
+  {
+    path: "/lazy3",
+    Component: LazyPage3,
+    name: "LazyPage-3",
+  },
+  {
+    path: "/lazy4",
+    Component: LazyPage3,
+    name: "LazyPage-4",
+  },
+];
