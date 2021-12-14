f26d5ac01e81cdd7586302c5165b3ad5e9881b6d
pabloc33 <pablo.contino33@gmail.com>
2021-12-13 23:20:14 -0300

    Fin del patron

--- src/02-components-patterns/components/ProductButtons.tsx
+++ src/02-components-patterns/components/ProductButtons.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { ProductContext } from "./ProductCard";
 import styles from "../styles/styles.module.css";
 
@@ -8,7 +8,15 @@ export interface Props {
 }
 
 export const ProductButtons = ({ className, style }: Props) => {
-  const { increaseBy, counter } = useContext(ProductContext);
+  // TODO: maxCount
+  const { increaseBy, counter, maxCount } = useContext(ProductContext);
+
+  const isMaxReached = useCallback(
+    () => !!maxCount && counter === maxCount,
+    [counter, maxCount]
+  );
+
+  // TODO: isMaxReached = useCallback, dependencias [counter, maxCount]
 
   return (
     <div className={`${styles.buttonsContainer} ${className}`} style={style}>
@@ -16,7 +24,10 @@ export const ProductButtons = ({ className, style }: Props) => {
         -
       </button>
       <div className={styles.countLabel}>{counter}</div>
-      <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>
+      <button
+        className={`${styles.buttonAdd} ${isMaxReached() && styles.disabled}`}
+        onClick={() => increaseBy(+1)}
+      >
         +
       </button>
     </div>
